456f92a83084ad630a4154ec09ea82a90f3a5f21
Hafiz Furqan Ahmad <hfurqan.ai@gmail.com>
2023-09-23 22:55:40 +0500

    vanilla javascript -> jQuery

--- index.js
+++ index.js
@@ -4,78 +4,77 @@ let populateData = function (url, city_name) {
       return response.json();
     })
     .then((response) => {
-      for (let x of Object.keys(response)) {
-        try {
-          if (response["cod"] === "404") {
-            alert(response["message"]);
-            document.getElementById('country').value = "";
-            document.getElementById('country').blur();
-            document.getElementById('suggestions-list').style.display = "none";
-            document.getElementById('preloader').style.display = "none";
-            throw new Error("Error: Wrong City Entered");
-          }
-        } catch (error) {
-          console.log(error.message);
-          return;
+      try {
+        if (response["cod"] === "404") {
+          alert(response["message"]);
+          $('#country').val("");
+          $('#country').blur();
+          $('#suggestions-list').css('display', 'none');
+          $('#preloader').css('display', 'none');
+          throw new Error("Error: Wrong City Entered");
         }
-
-        document.querySelector(".location-name").innerHTML = city_name;
-        document.querySelector(".longitude").innerHTML = response["coord"]["lon"]
-        document.querySelector(".latitude").innerHTML = response["coord"]["lat"]
-        document.querySelector(".description").innerHTML = response["weather"]["0"]["main"] + ", " + response["weather"]["0"]["description"]
-        document.querySelector(".temp-deg").innerHTML = (parseFloat(response["main"]["temp"]) - 273.15).toFixed(1) + `<sup class="font-medium dark:text-white">&deg;</sup>`;
-        document.querySelector(".temp-fh").innerHTML = ((parseFloat(response["main"]["temp"]) - 273.15) * 9 / 5 + 32).toFixed(1) + "&deg;F";
-        document.querySelector(".min-temp").innerHTML = ((parseFloat(response["main"]["temp_min"]) - 273.15)).toFixed(1) + `<span class="font-medium">&deg;</span>`;
-        document.querySelector(".max-temp").innerHTML = ((parseFloat(response["main"]["temp_max"]) - 273.15)).toFixed(1) + `<span class="font-medium">&deg;</span>`;
-        document.querySelector(".feels-like").innerHTML = parseInt(response["main"]["feels_like"]) - 273 + "&deg;";
-
-        document.querySelector(".wind-speed").innerHTML = response["wind"]["speed"] + " mph"
-        document.querySelector(".wind-direction").innerHTML = response["wind"]["deg"] + "&deg;"
-        {
-          let riseTime = new Date(parseInt(response["sys"]["sunrise"]) * 1000);
-          document.querySelector(".sunrise").innerHTML = `${riseTime.getHours()}:${riseTime.getMinutes()} AM`
-        }
-        {
-          let setTime = new Date(parseInt(response["sys"]["sunset"]) * 1000);
-          let hours = setTime.getHours();
-          if (hours > 12) {
-            hours -= 12;
-          }
-          document.querySelector(".sunset").innerHTML = `${hours}:${setTime.getMinutes()} PM`
-        }
-        document.querySelector(".visibility").innerHTML = response["visibility"]
-        document.querySelector(".pressure").innerHTML = response["main"]["pressure"] + " hpa"
-        document.querySelector(".humidity").innerHTML = response["main"]["humidity"] + "%"
-        if (response["wind"]["gust"] === undefined) {
-          document.querySelector(".GUST").style.visibility = "hidden";
-        }
-        else {
-          document.querySelector(".GUST").style.visibility = "visible";
-          document.querySelector(".gusts").innerHTML = response["wind"]["gust"] + " mph"
-        }
+      } catch (error) {
+        console.log(error.message + "wronglocatin entered");
+        return;
       }
+
+      $(".location-name").html(city_name);
+      $(".longitude").html(response["coord"]["lon"]);
+      $(".latitude").html(response["coord"]["lat"]);
+      $(".description").html(response["weather"]["0"]["main"] + ", " + response["weather"]["0"]["description"]);
+      $(".temp-deg").html((parseFloat(response["main"]["temp"]) - 273.15).toFixed(1) + `<sup class="font-medium dark:text-white">&deg;</sup>`);
+      $(".temp-fh").html(((parseFloat(response["main"]["temp"]) - 273.15) * 9 / 5 + 32).toFixed(1) + "&deg;F");
+      $(".min-temp").html(((parseFloat(response["main"]["temp_min"]) - 273.15)).toFixed(1) + `<span class="font-medium">&deg;</span>`);
+      $(".max-temp").html(((parseFloat(response["main"]["temp_max"]) - 273.15)).toFixed(1) + `<span class="font-medium">&deg;</span>`);
+      $(".feels-like").html(parseInt(response["main"]["feels_like"]) - 273 + "&deg;");
+
+      $(".wind-speed").html(response["wind"]["speed"] + " mph");
+      $(".wind-direction").html(response["wind"]["deg"] + "&deg;");
+      {
+        let riseTime = new Date(parseInt(response["sys"]["sunrise"]) * 1000);
+        $(".sunrise").html(`${riseTime.getHours()}:${riseTime.getMinutes()} AM`);
+      }
+      {
+        let setTime = new Date(parseInt(response["sys"]["sunset"]) * 1000);
+        let hours = setTime.getHours();
+        if (hours > 12) {
+          hours -= 12;
+        }
+        $(".sunset").html(`${hours}:${setTime.getMinutes()} PM`);
+      }
+      $(".visibility").html(response["visibility"]);
+      $(".pressure").html(response["main"]["pressure"] + " hpa");
+      $(".humidity").html(response["main"]["humidity"] + "%");
+      if (response["wind"]["gust"] === undefined) {
+        $(".GUST").css('visibility', 'hidden');
+      }
+      else {
+        $(".GUST").css('visibility', 'visible');
+        $(".gusts").html(response["wind"]["gust"] + " mph");
+      }
+
       if (city_name === undefined) {
-        document.querySelector(".location-name").innerHTML = document.getElementById('country').value.charAt(0).toUpperCase() + document.getElementById('country').value.slice(1).toLowerCase();
+        $(".location-name").html($('#country').val().charAt(0).toUpperCase() + $('#country').val().slice(1).toLowerCase());
       }
 
-      document.getElementById('country').value = "";
-      document.getElementById('country').blur();
-      suggestionsList.style.display = "none";
-      document.getElementById('preloader').style.display = "none";
+      $('#country').val("");
+      $('#country').blur();
+      $('#suggestions-list').css('display', 'none');
+      $('#preloader').css('display', 'none');
     })
 }
 
-let search = document.getElementById('search');
-search.addEventListener('click', (e) => {
+
+$('#search').on('click', (e) => {
   e.preventDefault();
-  let country = document.getElementById('country').value;
+  let country = $('#country').val();
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=6618b265c24f3e0e2f3821f553b1a57e`;
   populateData(url);
 })
-country.addEventListener('keypress', (e) => {
+$('#country').on('keypress', (e) => {
   if (e.key === "Enter") {
     e.preventDefault();
-    let country = document.getElementById('country').value;
+    let country = $('#country').val();
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=6618b265c24f3e0e2f3821f553b1a57e`;
     populateData(url);
   }
@@ -84,24 +83,21 @@ $(document).ready(function () {
   console.log("JS is working properly!")
   let date = new Date();
   if (screen.width <= 768) {
-    let hide = document.querySelectorAll('.hide');
-    hide.forEach((elm) => {
-      elm.style.display = "none";
-    })
+    $('.hide').css('display', 'none');
   }
   else if (date.getHours() >= 6 && date.getHours() <= 18) {
-    document.querySelector('#sun').style.display = "block";
-    document.querySelector('#moon').style.display = "none";
+    $('#sun').css('display', 'block')
+    $('#moon').css('display', 'none')
   }
   else {
-    document.querySelector('#moon').style.display = "block";
-    document.querySelector('#sun').style.display = "none";
+    $('#moon').css('display', 'block')
+    $('#sun').css('display', 'none')
   }
 
   if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
+    $('html').addClass('dark');
   } else {
-    document.documentElement.classList.remove('dark')
+    $('html').removeClass('dark');
   }
 
   (function () {
@@ -137,50 +133,47 @@ $(document).ready(function () {
   })()
 })
 
-let checkbox = document.getElementById('checkbox');
-checkbox.addEventListener('click', (e) => {
-  document.getElementsByTagName('html')[0].classList.toggle('dark');
+$('#checkbox').on('click', (e) => {
+  $('html').toggleClass('dark');
 })
 
-window.addEventListener('keypress',(e)=>{
-  if(e.key === 'I' || e.key === 'i'){
-    document.getElementById('country').focus();
+window.addEventListener('keypress', (e) => {
+  if (e.key === 'I' || e.key === 'i') {
+    $('#country').focus();
   }
 })
 
 
 import states from './worldcities.js';
 
-const inputElement = document.getElementById('country');
-const suggestionsList = document.getElementById('suggestions-list');
-
-inputElement.addEventListener('input', ()=>{
-  const inputText = inputElement.value.trim().toLowerCase();
-  const filteredStates = states.filter((state)=>{
+$('#country').on('input', () => {
+  const inputText = $('#country').val().trim().toLowerCase();
+  const filteredStates = states.filter((state) => {
     return state.name.toLowerCase().includes(inputText);
   });
-  suggestionsList.innerHTML = "";
-  if(filteredStates.length > 0) {
-    filteredStates.forEach((state)=>{
-      const litElement = document.createElement('li');
-      litElement.classList.add("py-2","px-3","cursor-pointer","border-transparent","rounded-md","transition","dark:hover:bg-[#2d2d2d]","hover:bg-gray-400");
-      litElement.textContent = state.name;
-      litElement.addEventListener('click', ()=>{
-        inputElement.value = state.name;
-        suggestionsList.innerHTML = "";
-        document.getElementById('search').click();
+  $('#suggestions-list').html("");
+  if (filteredStates.length > 0) {
+    filteredStates.forEach((state) => {
+      const litElement = $('<li>');
+      litElement.addClass("py-2 px-3 cursor-pointer border-transparent rounded-md transition dark:hover:bg-[#2d2d2d] hover:bg-gray-400");
+      litElement.text(state.name);
+      litElement.on('click', () => {
+        $('#country').val(state.name);
+        $('#suggestions-list').html("");
+        $('#search').click();
       })
-      suggestionsList.appendChild(litElement);
+      $('#suggestions-list').append(litElement);
     })
-    suggestionsList.style.display = "block";
+    $('#suggestions-list').css('display', 'block');
   }
-  else{
-    suggestionsList.style.display = "none";
+  else {
+    $('#suggestions-list').css('display', 'none');
   }
 })
 
-document.addEventListener("click", function (e) {
-  if (!suggestionsList.contains(e.target) && e.target !== inputElement) {
-    suggestionsList.style.display = "none";
+
+$(document).on('click', function (e) {
+  if (!$(e.target).closest('#suggestions-list').length && e.target !== $('#country')[0]) {
+    $('#suggestions-list').css('display', 'none');
   }
 });
